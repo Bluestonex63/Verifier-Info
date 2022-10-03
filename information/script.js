42a9d6da8a3a3caf2ba199d4fc9c43e5a7433e8b
Bluestonex63 <103859845+Bluestonex63@users.noreply.github.com>
2022-10-04 00:03:08 +0100

    Added all the verifier related info

--- information/script.js
+++ information/script.js
@@ -2,10 +2,12 @@ document.querySelector("#separators").addEventListener("mousedown", function(e) 
     for (seperator of document.querySelectorAll(".separator")) {
         if (!(seperator.classList.contains("separatoractive") && (e.target == seperator || e.target == document.querySelector("#separators")))) {
             seperator.classList.remove("separatoractive")
+            document.querySelector(`#${seperator.id}div`).classList.add("invisible")
         }
     }
     if (!e.target.classList.contains("separatoractive") && e.target != document.querySelector("#separators")) {
         e.target.classList.add("separatoractive")
+        document.querySelector(`#${e.target.id}div`).classList.remove("invisible")
     }
 
 
