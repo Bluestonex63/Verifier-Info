9a07eec6b388d1ce054705d9d3bc78c75faa9267
Bluestonex63 <103859845+Bluestonex63@users.noreply.github.com>
2022-10-04 00:15:29 +0100

    Update api.js

--- api.js
+++ api.js
@@ -16,4 +16,5 @@ app.get('/style.css', function(req, res) {
 app.get('/script.js', function(req, res) {
     res.sendFile("information/sript.js");
 });
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+app.listen(3000);
+console.log(`App listening at http://localhost:3000`)
