213d259fb16ebac7df5047d552050f5673d029c2
Bluestonex63 <103859845+Bluestonex63@users.noreply.github.com>
2022-10-04 00:13:36 +0100

    adapted api to deno

--- api.js
+++ api.js
@@ -1,7 +1,15 @@
-const express = require('express');
-const port = 3000
-const app = express();
-app.use(express.static("information"))
+import {opine} from "https://deno.land/x/opine@2.3.3/mod.ts";
+const app = opine();
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'X-API-key,Content-Type');
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.sendFile("information/index.html");
+});
 app.get('/style.css', function(req, res) {
     res.sendFile("information/style.css");
 });
